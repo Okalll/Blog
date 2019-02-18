cce1083b86e51aafbf5b2e37729c054763559d3b
Okall Vivian <vivieokall@gmail.com>
2019-02-18 09:34:43 +0300

    "Add db"

--- app/models.py
+++ app/models.py
@@ -1,14 +1,14 @@
-from . import db, login_manager
+from . import db
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+# @login_manager.user_loader
+# def load_user(user_id):
+#     return User.query.get(int(user_id))
 
 
-class User(UserMixin, db.Model):
+class User(UserMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(8))
@@ -19,9 +19,9 @@ class User(UserMixin, db.Model):
     profile_pic_path = db.Column(db.String())
     pass_secure = db.Column(db.String(255))
 
-    @property
-    def password(self):
-        raise AttributeError('You cannot read the password attribute')
+@property
+def password(self):
+    raise AttributeError('You cannot read the password attribute')
 
     @password.setter
     def password(self, password):
